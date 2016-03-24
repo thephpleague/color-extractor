af2041b9553d3332a45d5dff373982108e374807
MatTheCat <math.lechat@gmail.com>
2016-03-24 13:45:57 +0100

    handle transparency

--- README.md
+++ README.md
@@ -48,6 +48,21 @@ $colors = $extractor->extract(5);
 
 ```
 
+## Handling transparency
+
+By default **any pixel with alpha value greater than zero will be discarded**. This is because transparent colors are not perceived
+as is. For exemple fully transparent black would be seen white on a white background. So if you want to take transparency into account
+when building a palette you have to specify this background color. You can do this with the second argument of `Palette` constructors.
+Its default value is `null`, meaning a color won't be added to the palette if its alpha component exists and is greater than zero.
+
+You can set it as an integer representing the color, then transparent colors will be blended before addition to the palette.
+
+```php
+// we set a white background so fully transparent colors will be added as white in the palette
+// pure red #FF0000 at 50% opacity will be stored as #FF8080 as it would be perceived
+$palette = Palette::fromFilename('./some/image.png', Color::fromHexToInt('#FFFFFF'));
+```
+
 ## Contributing
 
 Please see [CONTRIBUTING](https://github.com/thephpleague/color-extractor/blob/master/CONTRIBUTING.md) for details.
--- tests/League/ColorExtractor/Test/PaletteTest.php
+++ tests/League/ColorExtractor/Test/PaletteTest.php
@@ -2,6 +2,7 @@
 
 namespace League\ColorExtractor\Test;
 
+use League\ColorExtractor\Color;
 use League\ColorExtractor\ColorExtractor;
 use League\ColorExtractor\Palette;
 
@@ -10,6 +11,7 @@ class PaletteTest extends \PHPUnit_Framework_TestCase
     protected $jpegPath = './tests/assets/test.jpeg';
     protected $gifPath = './tests/assets/test.gif';
     protected $pngPath = './tests/assets/test.png';
+    protected $transparentPngPath = './tests/assets/red-transparent-50.png';
 
     public function testJpegExtractSingleColor()
     {
@@ -50,5 +52,16 @@ class PaletteTest extends \PHPUnit_Framework_TestCase
         $this->assertInternalType('array', $colors);
         $this->assertCount($numColors, $colors);
         $this->assertEquals($colors, [14024704, 3407872, 7111569]);
+    }
+
+    public function testTransparencyHandling()
+    {
+        $this->assertCount(0, Palette::fromFilename($this->transparentPngPath));
+
+        $whiteBackgroundPalette = Palette::fromFilename($this->transparentPngPath, Color::fromHexToInt('#FFFFFF'));
+        $this->assertEquals(iterator_to_array($whiteBackgroundPalette), [Color::fromHexToInt('#FF8080') => 1]);
+
+        $blackBackgroundPalette = Palette::fromFilename($this->transparentPngPath, Color::fromHexToInt('#000000'));
+        $this->assertEquals(iterator_to_array($blackBackgroundPalette), [Color::fromHexToInt('#7E0000') => 1]);
     }
 }
